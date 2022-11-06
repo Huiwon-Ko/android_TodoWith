e299ebc3fbd0411a8a90b5c9a767759815886c17
kryptonite43 <sonyj0430@gmail.com>
2022-11-06 21:45:43 +0900

    mongodb set - db upload (o) - getText from editText(x)

---
+++ app/src/main/java/com/example/todolist/TodoDetail.java
@@ -0,0 +1,13 @@
+package com.example.todolist;
+
+public class TodoDetail {
+    private String place;
+    private String date;
+    private String time;
+
+    public TodoDetail(String place, String date, String time) {
+        this.place = place;
+        this.date = date;
+        this.time = time;
+    }
+}
